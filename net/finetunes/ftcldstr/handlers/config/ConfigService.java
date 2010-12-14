2d82aed18df7c6bf0c88100039811f8e5c550eb0
Pavel Zakervashevich <pavel.zakervashevich@pms-software.com>
2010-12-14 21:30:52 +0200

    configs and comments.

---
+++ net/finetunes/ftcldstr/handlers/config/ConfigService.java
@@ -0,0 +1,16 @@
+package net.finetunes.ftcldstr.handlers.config;
+
+/**
+ * Config class to store global configuration values
+ *
+ */
+public class ConfigService {
+	
+	public static String getValue(String parameter) {
+		
+		// TODO: implement
+		return null;
+		
+	}
+
+}
--- net/finetunes/ftcldstr/handlers/request/RequestHandler.java
+++ net/finetunes/ftcldstr/handlers/request/RequestHandler.java
@@ -2,7 +2,12 @@ package net.finetunes.ftcldstr.handlers.request;
 
 public class RequestHandler {
 	
+	// main method to handle requests and
+	// call the request handler depending
+	// on the request type
 	public static void handleRequest() {
+		
+		// TODO: implement
 		
 	}
 
